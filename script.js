19c8611ffa31c9e32b6c2325f24e998ca3956f45
evg <shadjkez0r48@gmail.com>
2026-09-13 18:06:08 +0400

    feat: add style.css and dark/light theme throw scipts.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const themeSwitch = document.getElementById('theme-switch');
 const currentTheme = localStorage.getItem('theme');
 
+const updateIcon = (theme) => {
+  if (theme === 'dark') {
+    themeSwitch.textContent = '☀️ Light'; 
+  } else {
+    themeSwitch.textContent = '🌙 Dark'; 
+  }
+};
+
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme)
+    updateIcon(currentTheme);
+} else {
+    updateIcon('light');
 }
 
 themeSwitch.addEventListener('click', () => {
@@ -11,8 +22,10 @@ themeSwitch.addEventListener('click', () => {
     if (theme === 'dark') {
         document.documentElement.removeAttribute('data-theme');
         localStorage.setItem('theme', 'light');
+        updateIcon('light');
     } else {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
+        updateIcon('dark');
     }
 });
